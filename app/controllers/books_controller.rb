271de1452dbf6f2007cb9967d2e9fa5942fc4bc0
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-09-19 11:31:14 +0000

    [Update] 投稿時のサクセスメッセージ、失敗時のエラ〜メッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,10 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to books_path
+    redirect_to books_path, notice: 'successfully posted'
+    else
+     @books = Book.all
+     render :index
     end
   end
 
@@ -19,10 +22,20 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  # サクセスメッセージが機能しなかった時のため
+  # def update
+  #   book = Book.find(params[:id])
+  #   book.update(book_params)
+  #   redirect_to book_path(book)
+  # end
+
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+    redirect_to book_path(book), notice: 'successfully posted'
+    else
+      render :new
+    end
   end
 
   def destroy
