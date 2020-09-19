620dc8b0414f81dcb2f21e96fa2e776e2d70c4fe
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-09-19 04:59:45 +0000

    [Add] topからindex へのリンクの追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,19 +5,14 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-  end
-
-  def new
     @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to books_path(book.id)
-    else render "new"
-  end 
-  
+    redirect_to books_path
+    end
   end
 
   def edit
