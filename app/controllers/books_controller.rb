581e9d666ef8085c7839bf05b1e16fa496620958
EC2 Default User <ec2-user@ip-172-31-6-7.ec2.internal>
2020-09-20 07:09:38 +0000

    [Finish] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,11 +30,11 @@ class BooksController < ApplicationController
   # end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-    redirect_to book_path(book), notice: 'successfully posted'
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book), notice: 'successfully posted'
     else
-      render :new
+      render :edit
     end
   end
 
